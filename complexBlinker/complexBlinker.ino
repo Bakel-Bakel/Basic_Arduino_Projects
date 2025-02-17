d32851c4761766c5fc99d113cd7543232a15e71f
Bakel Bakel <begededum4bakel@gmail.com>
2025-02-17 23:44:21 +0100

    adding more projects

---
+++ complexBlinker/complexBlinker.ino
@@ -0,0 +1,62 @@
+int green = 2;
+int yellow = 3;
+int red = 4;
+
+void setup()
+{
+  
+  pinMode(green,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(red,OUTPUT);
+
+}
+
+void loop()
+
+{
+  for(int i=0 ; i<11 ; i++)
+  {
+    digitalWrite(green,HIGH);
+    digitalWrite(yellow,HIGH);
+    delay(100);
+    digitalWrite(green,LOW);
+    digitalWrite(yellow,LOW);
+    delay(100);
+  }
+  
+  for(int i=0 ; i<11 ; i++)
+  {
+    digitalWrite(green,HIGH);
+    digitalWrite(red,HIGH);
+    delay(100);
+    digitalWrite(green,LOW);
+    digitalWrite(red,LOW);
+    delay(100);
+    
+  }
+  
+  for(int i=0 ; i<11 ; i++)
+  {
+    digitalWrite(red,HIGH);
+    digitalWrite(yellow,HIGH);
+    delay(100);
+    digitalWrite(red,LOW);
+    digitalWrite(yellow,LOW);
+    delay(100);
+    
+  }
+  
+  for(int i=0 ; i<11 ; i++)
+  {
+    digitalWrite(green,HIGH);
+    digitalWrite(yellow,HIGH);
+    digitalWrite(red,HIGH);
+    delay(100);
+    digitalWrite(green,LOW);
+    digitalWrite(yellow,LOW);
+    digitalWrite(red,LOW);
+    delay(100);
+    
+  }
+
+}
